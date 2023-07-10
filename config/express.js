c8c66a4ed05326dc26bbcc48cfea9095ecf9d79a
eomgerm <fishbread00@gmail.com>
2023-07-10 20:26:40 +0900

    Fix: app export default로 변경

--- config/express.js
+++ config/express.js
@@ -1,26 +1,10 @@
 import express from "express";
 import cors from "cors";
 
-
-
-
-
-
-
-
-
-export const app = express();
+const app = express();
 const port = 3000;
 app.use(cors());
 
+app.get("/", (req, res) => res.send("Hello World!"));
 
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => res.send('Hello World!'));
+export default app;
