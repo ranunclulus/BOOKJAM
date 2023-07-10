a7e04dd33ff41ce7ddc1e30b225f8b7f7b0276f8
eomgerm <fishbread00@gmail.com>
2023-07-10 20:55:10 +0900

    Feat: response 함수 및 baseResponeStatus 추가

---
+++ config/baseResponeStatus.js
@@ -0,0 +1,6 @@
+const baseResponse = {
+  // Success
+  SUCCESS: { status: "SUCCESS", code: 1000, message: "성공" },
+};
+
+export default baseResponse;
--- config/express.js
+++ config/express.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { response } from "./response";
+import baseResponse from "./baseResponeStatus";
 
 const app = express();
 const port = 3000;
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.status(200).send(response(baseResponse.SUCCESS, "Hello World!")));
 
 export default app;
