693f67baf5572678773ac8ab7388e77364e67ac9
eomgerm <fishbread00@gmail.com>
2023-07-13 01:16:28 +0900

    Feat: places 라우터 추가

--- config/express.js
+++ config/express.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import { response } from "./response";
 import baseResponse from "./baseResponeStatus";
+import placesRouter from "../src/places/placesRouter";
 
 const app = express();
-const port = 3000;
 app.use(cors());
 
 app.get("/", (req, res) => res.status(200).send(response(baseResponse.SUCCESS, "Hello World!")));
+app.use("/places", placesRouter);
 
 export default app;
