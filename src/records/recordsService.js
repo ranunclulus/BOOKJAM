ef82c85f3c442bc21e78ccea71228875b3dff490
Chun Hyunwoo <1000chw@gmail.com>
2023-07-13 10:59:05 +0900

    Update recordsService.js
    getRecordsByUserId 수정

--- src/records/recordsService.js
+++ src/records/recordsService.js
@@ -6,8 +6,14 @@ const recordsService = {
             const connection = await pool.getConnection(async conn => conn);
             const recordsResult = await recordsDao.selectRecordsByUserId(connection, userId);
             connection.release();
+            
             if (recordsResult.error)
                 return {error: true}
+            
+            for (let i = 0; i < recordsResult.length; i++) {
+                recordsResult[i].images_url = recordsResult[i].images_url.split('|');
+            }
+
             return recordsResult;
         }catch(err) {
             return {error: true}
