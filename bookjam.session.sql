8896a78c77576b7aa4bf2894841cfb869cd9d11f
Chun Hyunwoo <1000chw@gmail.com>
2023-07-13 09:45:13 +0900

    Delete bookjam.session.sql

--- bookjam.session.sql
+++
@@ -1,3 +0,0 @@
-SELECT r.record_id, r.created_at, r.comment_count, r.like_count, ri.image_url
-FROM records AS r LEFT JOIN record_images AS ri ON ri.record_id = r.record_id AND r.author = 1
-GROUP BY r.record_id;
