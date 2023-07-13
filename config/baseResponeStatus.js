2a6e8167d3a22c0933fe0f1980f270e4b6bd587c
eomgerm <fishbread00@gmail.com>
2023-07-13 20:08:05 +0900

    Feat: 장소 리뷰 불러오기 구현

--- config/baseResponeStatus.js
+++ config/baseResponeStatus.js
@@ -7,10 +7,10 @@ const baseResponse = {
   SORT_BY_UNAVAILABLE: { status: "FAIL", code: 2001, message: "올바른 정렬 기준이 아닙니다." },
   LOCATION_EMPTY: { status: "FAIL", code: 2003, message: "위치 정보가 없습니다." },
   RECORDS_USERID_READ_FAIL: { status: "FAIL", code: 2004, message: "기록 - 유저 아이디 읽기 실패" },
+  PLACE_NOT_FOUND: { status: "FAIL", code: 2005, message: "장소가 존재하지 않습니다." },
 
   //ERROR
   SERVER_ERROR: { status: "ERROR", code: 3000, message: "서버 에러" },
-
 };
 
 export default baseResponse;
