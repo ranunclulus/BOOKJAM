c8124acf7058e2f53b0f5f72abf6d756bd919a06
Chun Hyunwoo <1000chw@gmail.com>
2023-07-13 23:43:30 +0900

    Feat: 8- POST /records/:userId
    userId의 기록 저장하기 - 이미지는 지정 경로에 저장 후 url을 db에 저장

--- src/records/recordsController.js
+++ src/records/recordsController.js
@@ -6,19 +6,48 @@ import recordsProvider from "./recordsProvider";
 const recordsController = {
     getRecordsByUserId: async (req, res) => {
         try {
-            const userId = req.params.userId;
+            const userId = Number(req.params.userId);
             if (!userId) {
                 return res.status(400).json(response(baseResponse.RECORDS_USERID_READ_FAIL));
             }
             const records = await recordsService.getRecordsByUserId(userId);
             if (records.error)
-                return res.status(400).json(response(baseResponse.SERVER_ERROR));
+                return res.status(500).json(response(baseResponse.SERVER_ERROR));
             return res.status(200).json(response(baseResponse.SUCCESS, records));
         } catch (error){
             console.log(error);
-            return res.status(400).json(response(baseResponse.SERVER_ERROR));
+            return res.status(500).json(response(baseResponse.SERVER_ERROR));
+        }
+    },
+
+    postRecords: async (req, res) => {
+        try{
+            const userId = Number(req.params.userId);
+            if (!userId) {
+                return res.status(400).json(response(baseResponse.RECORDS_USERID_READ_FAIL));
+            }
+            let {place, isbn, date, emotions, activity, contents, isNotPublic, comment_not_allowed} = req.body;
+            const photos = req.files;
+            const images_url = [];
+            for(let i = 0; i < photos.length; i++){
+                images_url.push(photos[i].path);
+            }
+            place = Number(place);
+            emotions = Number(emotions);
+            activity = Number(activity);
+            isNotPublic = Number(isNotPublic);
+            comment_not_allowed = Number(comment_not_allowed);
+            const recordData = [userId, place, isbn, date, activity, emotions, contents, isNotPublic, comment_not_allowed];
+            const records = await recordsProvider.postRecord(recordData, images_url);
+            if (records.error)
+                return res.status(500).json(response(baseResponse.SERVER_ERROR));
+            return res.status(200).json(response(baseResponse.SUCCESS));
+        } catch (error){
+            console.log(error);
+            return res.status(500).json(response(baseResponse.SERVER_ERROR));
         }
     }
+
 }
 
 export default recordsController;
--- src/records/recordsProvider.js
+++ src/records/recordsProvider.js
@@ -1,7 +1,17 @@
 import pool from "../../config/database";
+import recordsDao from "./recordsDao";
 
 const recordsProvider = {
-
+    postRecord: async (recordData, images_url) => {
+        try {
+            const connection = await pool.getConnection();
+            const record = await recordsDao.insertRecord(connection, recordData, images_url);
+            connection.release();
+            return record;
+        } catch (error) {
+            return {error: true}
+        }
+    },
 }
 
 export default recordsProvider;
