a1293c3f892411ba4b812ff71904f253e7b12533
eomgerm <fishbread00@gmail.com>
2023-07-14 00:41:04 +0900

    Feat: reviewsRouter 추가

--- config/express.js
+++ config/express.js
@@ -4,13 +4,15 @@ import { response } from "./response";
 import baseResponse from "./baseResponeStatus";
 import recordsRouter from "../src/records/recordsRouter";
 import placesRouter from "../src/places/placesRouter";
+import reviewsRouter from "../src/reviews/reviewsRouter";
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => res.status(200).send(response(baseResponse.SUCCESS, "Hello World!")));
-app.use("/places", placesRouter);
 
+app.use("/places", placesRouter);
 app.use("/records", recordsRouter);
+app.use("/reviews", reviewsRouter);
 
 export default app;
