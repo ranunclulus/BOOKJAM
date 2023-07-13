ba62191081b3371ad39b96807d1f25977185bd28
ranunclulus <morion002@gmail.com>
2023-07-14 08:28:35 +0900

    Feat: 장소 아이디를 입력받았을 때 해당 장소에 대한 활동 다 보여 주는 기능 구현

--- src/Activity/activityController.js
+++ src/Activity/activityController.js
@@ -3,12 +3,13 @@ import activityProvider from "./activityProvider";
 import { response } from "../../config/response";
 import baseResponse from "../../config/baseResponeStatus";
 
-/**
- * API No. 1
- * API Name : 활동 세부 정보 조회
- * [GET] /places/activities/{activityId}
- */
+
 const activityController = {
+    /**
+     * API No. 1
+     * API Name : 활동 세부 정보 조회
+     * [GET] /places/activities/{activityId}
+     */
     getActivityByActivityId: async (req, res) => {
         try {
             const activityId = req.params.activityId;
@@ -26,6 +27,26 @@ const activityController = {
             return res.status(500).json(response(baseResponse.SERVER_ERROR));
 
 
+        }
+    },
+    /**
+     * API No. 2
+     * API Name : 장소에 해당하는 활동들 전부 조회
+     * [GET] /places/{placeId}/acrivities
+     */
+    getActivitiesByPlaceId: async (req, res) => {
+        try {
+            const placeId = req.params.placeId;
+            
+            const activities = await activityProvider.retrieveActivitiesByPlaceId(placeId);
+
+            if(activities.error) {
+                return res.status(400).json(response(baseResponse.LOCATION_EMPTY));
+            } 
+            return res.status(200).json(response(baseResponse.SUCCESS, activities))
+        } catch (error) {
+            //console.log(error);
+            return res.status(500).json(response(baseResponse.SERVER_ERROR));
         }
     }
 }
--- src/Activity/activityProvider.js
+++ src/Activity/activityProvider.js
@@ -1,11 +1,12 @@
 import recordsDao from "./activityDao";
 import pool from "../../config/database";
+import activityDao from "./activityDao";
 
 const activityProvider = {
     retrieveActivityByActivityId: async (activityId) => {
         try{
             const connection = await pool.getConnection(async conn => conn);
-            const activityResult = await recordsDao.selectActivityByActivityId(connection, activityId);
+            const activityResult = await activityDao.selectActivityByActivityId(connection, activityId);
 
             if(Object.keys(activityResult).length === 0) {
                 return {error: true};
@@ -18,7 +19,27 @@ const activityProvider = {
         } catch(err) {
             return {error: true};
         }
+    }, 
+
+    retrieveActivitiesByPlaceId: async (placeId) => {
+        try {
+            const connection = await pool.getConnection(async conn => conn);
+            const activitiesResult = await activityDao.selectActivitiesByPlaceId(connection, placeId);
+            console.log(activitiesResult);
+            console.log(typeof(activitiesResult));
+            if(Object.keys(activitiesResult).length == 0) {
+                return {error: true};
+            }
+            connection.release();
+            return {
+                error: false,
+                result: activitiesResult
+            };
+        } catch (err) {
+            return {error: true};
+        }
     }
+
 }
 
 export default activityProvider;
