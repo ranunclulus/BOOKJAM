1d0745694eaa2cd00587a0e3cab8b98c0d0847ef
eomgerm <fishbread00@gmail.com>
2023-07-14 21:20:34 +0900

    FEAT: auth Router 추가

--- config/express.js
+++ config/express.js
@@ -6,16 +6,20 @@ import recordsRouter from "../src/records/recordsRouter";
 import placesRouter from "../src/places/placesRouter";
 import reviewsRouter from "../src/reviews/reviewsRouter";
 import activityRoute from "../src/Activity/activityRoute";
+import authRouter from "../src/auth/authRouter";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.status(200).send(response(baseResponse.SUCCESS, "Hello World!")));
 
 app.use("/places", placesRouter);
 app.use("/records", recordsRouter);
 app.use("/reviews", reviewsRouter);
-app.use(activityRoute)
+app.use("/auth", authRouter);
+app.use(activityRoute);
 
 export default app;
