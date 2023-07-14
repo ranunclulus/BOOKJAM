62818a1fdf3d0bb7c983440a0afced34d7aa0d9e
eomgerm <fishbread00@gmail.com>
2023-07-14 21:20:14 +0900

    FEAT: 이메일 형식이 아닌 경우 반환하는 에러 추가

--- config/baseResponeStatus.js
+++ config/baseResponeStatus.js
@@ -1,17 +1,18 @@
 const baseResponse = {
   // Success
   SUCCESS: { status: "SUCCESS", code: 1000, message: "성공" },
-  
+
   //FAIL
   SEARCH_KEYWORD_EMPTY: { status: "FAIL", code: 2000, message: "검색 키워드를 입력해야 합니다." },
   SORT_BY_UNAVAILABLE: { status: "FAIL", code: 2001, message: "올바른 정렬 기준이 아닙니다." },
   LOCATION_EMPTY: { status: "FAIL", code: 2003, message: "위치 정보가 없습니다." },
   RECORDS_USERID_READ_FAIL: { status: "FAIL", code: 2004, message: "기록 - 유저 아이디 읽기 실패" },
   PLACE_NOT_FOUND: { status: "FAIL", code: 2005, message: "장소가 존재하지 않습니다." },
-  NOT_FRIEND: { status: "FAIL", code: 2006, message: "친구가 아닌 유저입니다."},
-  USER_NOT_FOUND: { status: "FAIL", code: 2007, message: "존재하지 않는 유저입니다."},
+  NOT_FRIEND: { status: "FAIL", code: 2006, message: "친구가 아닌 유저입니다." },
+  USER_NOT_FOUND: { status: "FAIL", code: 2007, message: "존재하지 않는 유저입니다." },
   REVIEW_NOT_FOUND: { status: "FAIL", code: 2008, message: "리뷰가 존재하지 않습니다." },
   ACTIVITY_ACTIVITYID_EMPTY: { status: "FAIL", code: 2100, message: "존재하지 않는 activityId" },
+  NOT_EMAIL_EXP: { status: "FAIL", code: "2009", message: "이메일 형식이 아닙니다." },
 
   //ERROR
   SERVER_ERROR: { status: "ERROR", code: 3000, message: "서버 에러" },
