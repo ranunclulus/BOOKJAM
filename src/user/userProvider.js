81dc2d6140ea62695d9cc8f9c93466a4bd8f004f
Chun Hyunwoo <1000chw@gmail.com>
2023-07-15 18:25:02 +0900

    Feat: 24 - user PUT username, profile, password, disabled

--- src/user/userProvider.js
+++ src/user/userProvider.js
@@ -2,16 +2,65 @@ import pool from "../../config/database";
 import userDao from "./userDao";
 
 const userProvider = {
-    postRecord: async (recordData, images_url) => {
+    putUsername: async (userId, username) => {
         try {
             const connection = await pool.getConnection();
-            const record = await userDao.insertRecord(connection, recordData, images_url);
-            connection.release();
-            return record;
+            const result = await userDao.updateUsername(connection, userId, username);
+            connection.release()
+            if (result.error)
+                return {error: true}
+            return result;
         } catch (error) {
+            console.error(error);
+            return {error: true}
+        }
+        
+    },
+
+    putPassword: async (userId, password) => {
+        try {
+            const connection = await pool.getConnection();
+            const result = await userDao.updatePassword(connection, userId, password);
+            connection.release()
+            if (result.error)
+                return {error: true}
+            return result;
+        } catch (error) {
+            console.error(error);
+            return {error: true}
+        }
+        
+    },
+    
+    putProfile: async (userId, profileImg) => {
+        try {
+            const connection = await pool.getConnection();
+            const result = await userDao.updateProfile(connection, userId, profileImg);
+            connection.release()
+            if (result.error)
+                return {error: true}
+            return result;
+        } catch (error) {
+            console.error(error);
+            return {error: true}
+        }
+        
+    },
+    
+    putDisabled: async (userId) => {
+        try {
+            const connection = await pool.getConnection();
+            const result = await userDao.updateDisabled(connection, userId);
+            connection.release()  
+            if (result.error)
+                return {error: true} 
+            return result;
+        } catch (error) {
+            console.error(error);
             return {error: true}
         }
     },
+    
 }
 
 export default userProvider;
