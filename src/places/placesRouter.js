1528913b676ecf6b270f713e3322c12dac4ea4b2
eomgerm <fishbread00@gmail.com>
2023-07-17 02:04:10 +0900

    Feat: s3 업로드 미들웨어 추가

--- src/places/placesRouter.js
+++ src/places/placesRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import placesController from "./placesController";
+import middlewares from "../../config/middlewares";
 
 const placesRouter = express.Router();
 
 placesRouter.get("/search", placesController.searchPlaces);
 placesRouter.get("/:placeId(\\d+)/reviews", placesController.getReviews);
-placesRouter.post("/:placeId(\\d+)/reviews", placesController.postReview);
+placesRouter.post("/:placeId(\\d+)/reviews", middlewares.s3Upload.array("image", 5), placesController.postReview);
 
 export default placesRouter;
