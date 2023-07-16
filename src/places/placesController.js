c4265b61fac0af8e2b8c2dc85a4ef2af67f53185
eomgerm <fishbread00@gmail.com>
2023-07-16 20:31:19 +0900

    Feat: GET /places 카테고리 별 장소 API 구현

--- src/places/placesController.js
+++ src/places/placesController.js
@@ -2,6 +2,9 @@ import baseResponse from "../../config/baseResponeStatus";
 import { response } from "../../config/response";
 import placesService from "./placesService";
 
+const CATEGORY_KEYWORD = ["0", "1", "2"];
+const SORT_BY_KEYWORD = ["rating", "review", "distance"];
+
 const placesController = {
   searchPlaces: async (req, res) => {
     try {
@@ -13,9 +16,7 @@ const placesController = {
         return res.status(400).json(response(baseResponse.SEARCH_KEYWORD_EMPTY));
       }
 
-      const sortByKeyword = ["rating", "review", "distance"];
-
-      if (!sortByKeyword.includes(sortBy)) {
+      if (!SORT_BY_KEYWORD.includes(sortBy)) {
         return res.status(400).json(response(baseResponse.SORT_BY_UNAVAILABLE));
       }
 
@@ -46,6 +47,32 @@ const placesController = {
 
       return res.status(200).json(response(baseResponse.SUCCESS, reviews.result));
     } catch (error) {
+      return res.status(500).json(response(baseResponse.SERVER_ERROR));
+    }
+  },
+  getPlaces: async (req, res) => {
+    try {
+      const {
+        query: { category, sortBy = "rating", lat, lon, last },
+      } = req;
+
+      if (!CATEGORY_KEYWORD.includes(category)) {
+        return res.status(400).json(response(baseResponse.CATEGORY_UNAVAILABLE));
+      }
+
+      if (!SORT_BY_KEYWORD.includes(sortBy)) {
+        return res.status(400).json(response(baseResponse.SORT_BY_UNAVAILABLE));
+      }
+
+      if (sortBy === "distance" && !(lat & lon)) {
+        return res.status(400).json(response(baseResponse.LOCATION_EMPTY));
+      }
+
+      const places = await placesService.getPlacesByCategory(category, sortBy, { lat, lon }, last);
+
+      return res.status(200).json(response(baseResponse.SUCCESS, places));
+    } catch (error) {
+      console.log(error);
       return res.status(500).json(response(baseResponse.SERVER_ERROR));
     }
   },
