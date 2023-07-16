464b1d82b71aecaefd77c92b994795c3ca69b7a4
eomgerm <fishbread00@gmail.com>
2023-07-17 00:49:24 +0900

    Feat: s3Upload middleware 추가

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,11 +1,8 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "files/excels"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
-});
-
-const s3 = new aws.S3({
+const storage = new aws.S3({
   credentials: {
     accessKeyId: privateInfo.AWS_ID,
     secretAccessKey: privateInfo.AWS_SECRET,
@@ -14,7 +11,19 @@ const s3 = new aws.S3({
 
 const multerS3Uploader = multerS3({
   s3,
-  bucket: "rentalbox",
+  bucket: "bookjam-bucket",
   key: (req, file, cb) => cb(null, file.originalname),
   acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key: (req, file, cb) => cb(null, `${Date.now()}_${file.originalname}`),
 });
+
+const middlewares = {
+  s3Upload: multer({
+    dest: "uploads/images/",
+    limits: {
+      fieldSize: 5 * 1024 * 1024,
+    },
+    storage: multerS3Uploader,
+  }),
+};
