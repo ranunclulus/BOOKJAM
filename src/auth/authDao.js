55c3e67288212cca3a7277ff09fb97b9b455eeaf
ranunclulus <morion002@gmail.com>
2023-08-16 19:41:53 +0900

    !HOTFIX: authDao 중복 코드 삭제

--- src/auth/authDao.js
+++ src/auth/authDao.js
@@ -1,19 +1,6 @@
 import logger from "../../config/logger";
 
 const authDao = {
-  selectUserByEmail: async (email, connection) => {
-    const sql = `
-      select user_id
-      from users
-      where email = '${email}'
-      limit 1
-    `;
-
-    const [queryResult] = await connection.query(sql);
-
-    return queryResult;
-  },
-
   selectThreeRandomUsers: async (connection) => {
     const sql = "select user_id, name, email, username, profile_image, username from users where disabled_at IS NULL order by rand() limit 3";
     const [queryResult] = await connection.query(sql);
